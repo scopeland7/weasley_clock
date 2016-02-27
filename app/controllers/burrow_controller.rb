1393b909916a2e7d0691bd744725cac48e049924
Sam Copeland <samcopeland7@gmail.com>
2016-02-27 11:14:45 -0500

    trying the Rack::Request object again...

--- app/controllers/burrow_controller.rb
+++ app/controllers/burrow_controller.rb
@@ -1,6 +1,11 @@
 class BurrowController < ApplicationController
   def index
-  	@result = request.location
+  	# @result = Geocoder.location("96.83.4.113")
+  	@request = request.safe_location 
+  	@result = HTTParty.get("http://ip-api.com/json/96.83.4.113")
+  	@lat = @result['lat']
+  	@lon = @result['lon']
+  	@address = Geocoder.address("#{@lat}, #{@lon}")
   end
 
   def members
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,11 @@
 class Location < ActiveRecord::Base
+	
+	reverse_geocoded_by :latitude, :longitude do |obj,results|
+  if geo = results.first
+    obj.city    = geo.city
+    obj.zipcode = geo.postal_code
+    obj.country = geo.country_code
+  end
+end
+after_validation :reverse_geocode
 end
