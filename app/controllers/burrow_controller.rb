69ba18b331d7a5a2ed29cac9e72f48ad2593ee6c
Sam Copeland <samcopeland7@gmail.com>
2016-02-29 11:39:54 -0500

    Javascript successfully gets current location

--- app/controllers/burrow_controller.rb
+++ app/controllers/burrow_controller.rb
@@ -1,11 +1,5 @@
 class BurrowController < ApplicationController
   def index
-  	# @result = Geocoder.location("96.83.4.113")
-  	# @request = request.safe_location 
-  	# @result = HTTParty.get("http://ip-api.com/json/96.83.4.113")
-  	# @lat = 34.1268
-  	# @lon = -84.574
-  	# @address = Geocoder.address("#{@lat}, #{@lon}")
   end
 
   def members
