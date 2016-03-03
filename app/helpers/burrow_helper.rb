030a0796a179fe4d7de87308d1e5ad4c386ae413
Sam Copeland <samcopeland7@gmail.com>
2016-03-03 07:13:08 -0500

    installed degrees to UTM converter gem

--- app/helpers/burrow_helper.rb
+++ app/helpers/burrow_helper.rb
@@ -1,2 +1,14 @@
 module BurrowHelper
+
+	def coords_to_utm(lat, lon)
+		utm = GeoUtm::LatLon.new(lat, lon)
+		return [utm.e, utm.n]
+	end
+
+	def dist_btw_coords(lat1, lon1, lat2, lon2)
+		coords1 = coords_to_utm(lat1, lon1)
+		coords2 = coords_to_utm(lat2, lon2)
+		
+	end
+
 end
